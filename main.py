d876e6888c098d44cc63b079051f3e6e73bd4352
FelippeTN <felippenalim2004@gmail.com>
2025-03-22 17:02:26 -0300

    REFACTOR: Refactored for deployment on Railway.

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import datetime, time, pytz, schedule
 from app.asset_report import *
 
-if __name__ == "__main__":
+brt = pytz.timezone('America/Sao_Paulo')
+
+def perform_task():
+    now = datetime.datetime.now(brt).strftime('%Y-%m-%d %H:%M:%S')
+    print(f"[{now}] Performing Task...")
     asset_name, links = url_build()
     web_scrapping(asset_name, links)
+
+schedule.every().day.at("09:00").do(perform_task)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)  
